3182e7dd9cdff09cc61d925b4dfa92bf0860a3f5
chrisar12 <amchrisar@gmail.com>
2017-12-11 00:31:31 -0600

    login

--- SituacionSalud/urls.py
+++ SituacionSalud/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
+from django.contrib.auth.views import login
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.login),
+   # url(r'^$', login, {'template_name':'login.html'}, name='login'),
     #url(r'^$', views.login),
-    # url(r'^$', views.index, name='index'),
-    url(r'^index/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+   # url(r'^index/$', views.index, name='index'),
     url(r'^factorescondicionantes/$', views.factorescondicionantes, name='factorescondicionantes'),
 
 
--- Tesis/urls.py
+++ Tesis/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.contrib.auth.views import login
 
 
 urlpatterns = [
-    url(r'', include('SituacionSalud.urls')),
+    url(r'^SituacionSalud/', include('SituacionSalud.urls', namespace='SituacionSalud')),
+    url(r'^$', login, {'template_name': 'situacionsalud/login.html'}, name='login'),
+
     # Admin
     url('admin/', admin.site.urls),
 
