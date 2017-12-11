005d5628ba10d4c015ff7f4672ecc676b81c5170
chrisar12 <amchrisar@gmail.com>
2017-12-11 12:38:25 -0600

    modelos

--- SituacionSalud/admin.py
+++ SituacionSalud/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-#from .models import Cliente
+from .models import *
 
 # Register your models here.
 
-#admin.site.register(Cliente)
+
+admin.site.register(EstablecimientoSalud)
+admin.site.register(Zona)
+admin.site.register(NivelEconomico)
+admin.site.register(Seguro)
+admin.site.register(Cie)
+admin.site.register(Distrito)
+admin.site.register(Sexo)
+admin.site.register(SituacionSalud)
--- SituacionSalud/models.py
+++ SituacionSalud/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+class EstablecimientoSalud(models.Model):
+    codigo = models.IntegerField(blank=True, null=True)
+    descripcion = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.codigo
+
+class Distrito(models.Model):
+    nombre = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.nombre
+
+class Seguro(models.Model):
+    nombre = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.nombre
+
+class Cie(models.Model):
+    codigo = models.IntegerField(blank=True, null=True)
+    descripcion = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.codigo
+
+class Sexo(models.Model):
+    nombre = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.nombre
+
+class NivelEconomico(models.Model):
+    nombre = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Zona(models.Model):
+    nombre = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.nombre
+
+class SituacionSalud(models.Model):
+    edad = models.IntegerField(blank=True, null=True)
+    sexo = models.ForeignKey(Sexo)
+    distrito = models.ForeignKey(Distrito)
+    cie = models.ForeignKey(Cie)
+    seguro = models.ForeignKey(Seguro)
+    niveleconomico = models.ForeignKey(NivelEconomico)
+    zona = models.ForeignKey(Zona)
+    eess = models.ForeignKey(EstablecimientoSalud)
+
+
+    def __str__(self):
+        return self.edad
