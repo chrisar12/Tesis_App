b0b8e83f3068ab147f1426e94bd8c17f34b87d03
chrisar12 <amchrisar@gmail.com>
2017-12-11 02:09:42 -0600

    login

--- Tesis/settings.py
+++ Tesis/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'situacionsalud',
-        'USER': 'postgres',
+        'USER': 'cam',
         'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
