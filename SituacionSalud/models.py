d7e533728cb9f187f0072c5039387d53bd4f05ec
chrisar12 <amchrisar@gmail.com>
2017-12-12 15:56:34 -0600

    modelos 3

--- SituacionSalud/models.py
+++ SituacionSalud/models.py
@@ -10,12 +10,6 @@ class prueba(models.Model):
     def __str__(self):
         return self.codigo
 
-class EstablecimientoSalud(models.Model):
-    codigo = models.IntegerField(blank=True, null=True)
-    descripcion = models.CharField(max_length=210)
-
-    def __str__(self):
-        return self.codigo
 
 class Distrito(models.Model):
     nombre = models.CharField(max_length=210)
@@ -30,7 +24,14 @@ class Seguro(models.Model):
         return self.nombre
 
 class Cie(models.Model):
-    codigo = models.CharField(max_length=50)
+    codigo = models.CharField(max_length=50,primary_key=True)
+    descripcion = models.CharField(max_length=210)
+
+    def __str__(self):
+        return self.codigo
+
+class EstablecimientoSalud(models.Model):
+    codigo = models.IntegerField(primary_key=True)
     descripcion = models.CharField(max_length=210)
 
     def __str__(self):
@@ -57,13 +58,15 @@ class Zona(models.Model):
 
 class SituacionSalud(models.Model):
     edad = models.IntegerField(blank=True, null=True)
-    sexo = models.ForeignKey(Sexo)
-    distrito = models.ForeignKey(Distrito)
-    cie = models.ForeignKey(Cie)
-    seguro = models.ForeignKey(Seguro)
-    niveleconomico = models.ForeignKey(NivelEconomico)
-    zona = models.ForeignKey(Zona)
-    eess = models.ForeignKey(EstablecimientoSalud)
+    sexo = models.ForeignKey(Sexo,null=True, blank=True)
+    #sexoden = models.CharField(max_length=20,  null=True)
+    distrito = models.ForeignKey(Distrito,null=True, blank=True)
+    #distritoden = models.CharField(max_length=100,  null=True)
+    cie = models.ForeignKey(Cie,null=True, blank=True)
+    seguro = models.ForeignKey(Seguro,null=True, blank=True)
+    niveleconomico = models.ForeignKey(NivelEconomico,null=True, blank=True)
+    zona = models.ForeignKey(Zona,null=True, blank=True)
+    eess = models.ForeignKey(EstablecimientoSalud,null=True, blank=True)
 
 
     def __str__(self):
