974950bfbc902afdf6e12c385d183f2d26a175d0
Junior <freesoftandskate@gmail.com>
2017-12-19 21:15:44 -0500

    cri cri ya esta :)

--- SituacionSalud/models.py
+++ SituacionSalud/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -17,18 +18,21 @@ class Distrito(models.Model):
     def __str__(self):
         return self.nombre
 
+
 class Seguro(models.Model):
     nombre = models.CharField(max_length=210)
 
     def __str__(self):
         return self.nombre
 
+
 class Cie(models.Model):
-    codigo = models.CharField(max_length=50,primary_key=True)
+    codigo = models.CharField(max_length=50, primary_key=True)
     descripcion = models.CharField(max_length=210)
 
     def __str__(self):
         return self.codigo
+
 
 class EstablecimientoSalud(models.Model):
     codigo = models.IntegerField(primary_key=True)
@@ -37,11 +41,14 @@ class EstablecimientoSalud(models.Model):
     def __str__(self):
         return self.codigo
 
+
 class Sexo(models.Model):
     nombre = models.CharField(max_length=210)
+    abreviacion = models.CharField(max_length=5, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
+
 
 class NivelEconomico(models.Model):
     nombre = models.CharField(max_length=210)
@@ -56,18 +63,27 @@ class Zona(models.Model):
     def __str__(self):
         return self.nombre
 
-class SituacionSalud(models.Model):
-    edad = models.IntegerField(blank=True, null=True)
-    sexo = models.ForeignKey(Sexo,null=True, blank=True)
-    #sexoden = models.CharField(max_length=20,  null=True)
-    distrito = models.ForeignKey(Distrito,null=True, blank=True)
-    #distritoden = models.CharField(max_length=100,  null=True)
-    cie = models.ForeignKey(Cie,null=True, blank=True)
-    seguro = models.ForeignKey(Seguro,null=True, blank=True)
-    niveleconomico = models.ForeignKey(NivelEconomico,null=True, blank=True)
-    zona = models.ForeignKey(Zona,null=True, blank=True)
-    eess = models.ForeignKey(EstablecimientoSalud,null=True, blank=True)
 
+class Edad(models.Model):
+    tipo = models.CharField(max_length=2)
+    edad = models.IntegerField()
+    nomenclaturar = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.edad
+        return str(self.edad.edad)
+
+
+class SituacionSalud(models.Model):
+    edad = models.ForeignKey(Edad, null=True, blank=True)
+    sexo = models.ForeignKey(Sexo, null=True, blank=True)
+    # sexoden = models.CharField(max_length=20,  null=True)
+    distrito = models.ForeignKey(Distrito, null=True, blank=True)
+    # distritoden = models.CharField(max_length=100,  null=True)
+    cie = models.ForeignKey(Cie, null=True, blank=True)
+    seguro = models.ForeignKey(Seguro, null=True, blank=True)
+    niveleconomico = models.ForeignKey(NivelEconomico, null=True, blank=True)
+    zona = models.ForeignKey(Zona, null=True, blank=True)
+    eess = models.ForeignKey(EstablecimientoSalud, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.edad.edad)
--- SituacionSalud/views.py
+++ SituacionSalud/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .models import Cie
+from pip.cmdoptions import no_binary
+
+from .models import *
 from .models import SituacionSalud
 from .forms import SituacionSaludForm
 
@@ -48,20 +50,96 @@ import csv
 import os
 from django.conf import settings
 
-path = os.path.join(settings.BASE_DIR, 'static/cie.csv')
+path_cie = os.path.join(settings.BASE_DIR, 'static/cie.csv')
+path_distr = os.path.join(settings.BASE_DIR, 'static/distrito.csv')
+path_esta = os.path.join(settings.BASE_DIR, 'static/EstablecimientoSalud.csv')
+path_situacion = os.path.join(settings.BASE_DIR, 'static/situacionsalud.csv')
 
 
-def solucionarspacios():
+def migrar_cie():
     try:
-        ob = Cie.objects.all()
-        url = path
+        ob = Cie.objects.all().delete()
+        url = path_cie
         with open(url) as csvarchivo:
             entrada = csv.reader(csvarchivo)
             for i in entrada:
                 cod, desc = i
                 cie = Cie(codigo=str(cod).strip().upper(),
-                          descripcion=str(desc).strip().upper())
+                          descripcion=str(desc).strip())
                 cie.save()
 
     except Exception:
+        print('error cie')
+
+
+def migrar_sexo():
+    try:
+        ob = Sexo.objects.all()
+        sf = Sexo(nombre='Masculino', abreviacion='M')
+        sf.save()
+        sm = Sexo(nombre='Femenino', abreviacion='F')
+        sm.save()
+    except Exception:
         print('error')
+
+
+def migrar_distrito():
+    try:
+        ob = Distrito.objects.all().delete()
+        url = path_distr
+        with open(url) as csvarchivo:
+            entrada = csv.reader(csvarchivo)
+            for i in entrada:
+                cod, num = i
+                cie = Distrito(nombre=str(cod).strip())
+                cie.save()
+
+    except Exception:
+        print('error distrito')
+
+
+def migrar_establecimiento():
+    try:
+        ob = EstablecimientoSalud.objects.all().delete()
+        url = path_esta
+        with open(url) as csvarchivo:
+            entrada = csv.reader(csvarchivo)
+            for i in entrada:
+                ID, EESS = i
+                cie = EstablecimientoSalud(codigo=str(ID).strip().upper(),
+                                           descripcion=str(EESS).strip())
+                cie.save()
+
+    except Exception:
+        print('error EstablecimientoSalud')
+
+
+def migrar_situacionsalud():
+    try:
+        ob = SituacionSalud.objects.all().delete()
+        url = path_situacion
+        with open(url) as csvarchivo:
+            entrada = csv.reader(csvarchivo)
+            for i in entrada:
+                id, persons, edd, cod, sex, distrit = i
+
+                if 0 <= int(edd) <= 29:
+                    edad_o = Edad(edad=int(edd), tipo='D', nomenclaturar='Dias')
+                if 51 <= int(edd) <= 61:
+                    edad_o = Edad(edad=int(edd), tipo='M', nomenclaturar='Meses')
+                if 100 <= int(edd):
+                    edad_o = Edad(edad=int(int(edd) - 100), tipo='A', nomenclaturar='Años')
+
+                edad_o.save()
+
+                ees_o = EstablecimientoSalud.objects.filter(codigo=str(id).strip().upper()).first()
+                cie_o = Cie.objects.filter(codigo=str(cod).strip().upper()).first()
+                distrit_o = Distrito.objects.filter(nombre=str(distrit).strip()).first()
+                sex_o = Sexo.objects.filter(abreviacion=str(sex).strip().upper()).first()
+
+                situsal = SituacionSalud(eess=ees_o, cie=cie_o, distrito=distrit_o, sexo=sex_o, edad=edad_o)
+                print(situsal)
+                situsal.save()
+
+    except Exception:
+        print('error SituacionSalud')
